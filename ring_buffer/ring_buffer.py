354dc6a048e1be159f82f1e8a721c95c26d1d9e9
HitzeD <d-hitze@comcast.net>
2019-09-20 11:21:55 -0400

    finished get

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,31 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+      if len(self.storage) == self.current:
+        print('here!')
+      else:
+        self.storage.append(item)
 
   def get(self):
-    pass
+    return self.storage
+
+# ********* TESTS ************
+
+buffer = RingBuffer(3)
+
+# buffer.get() # returns []
+
+buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+print(buffer.get()) # returns [a, b, c]
+
+# buffer.append('d')
+
+# buffer.get # returns [d, b, c]
+
+# buffer.append('e')
+# buffer.append('f')
+
+# buffer.get() # returns [d, e, f]
